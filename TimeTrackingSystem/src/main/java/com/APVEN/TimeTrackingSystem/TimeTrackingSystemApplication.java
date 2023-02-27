0a99b9d0737d48f5eac7827bc6e1ccc10f49a7cc
bestappsdevelopersteam <bestappsdevelopersteam@gmail.com>
2023-02-27 09:33:20 +0200

    Start project with Spring Initializer Java17,Maven2.7.9

---
+++ TimeTrackingSystem/src/main/java/com/APVEN/TimeTrackingSystem/TimeTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.APVEN.TimeTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ TimeTrackingSystem/src/test/java/com/APVEN/TimeTrackingSystem/TimeTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APVEN.TimeTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
